77b2e750f2fe035d5aff126c45743c6be5c544ce
Zulfia Nazar Gul Saleem Gul <zulfiyeselim05@gmail.com>
2026-07-28 17:32:14 +0300

    Add package installation requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 import setuptools
 
+
 setuptools.setup(
     name="novavision-secret-output-viewer",
-    version="0.2.0",
+    version="0.2.1",
     author="DigiNova",
     author_email="info@diginova.com.tr",
-    description="Trusted consumer for Environment Secrets Store references.",
+    description=(
+        "Trusted consumer for Environment Secrets Store references."
+    ),
     license="MIT",
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "pytest>=8,<9",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,6 +27,10 @@ setuptools.setup(
         "novavision.package.models",
         "novavision.package.utils",
     ],
-    package_dir={"novavision.package": "src"},
+    package_dir={
+        "novavision.package": "src",
+    },
     python_requires=">=3.8",
+    include_package_data=True,
+    zip_safe=False,
 )
